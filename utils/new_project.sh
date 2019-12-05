f075d11624c3b3ac8d012085cba21504a11b854c
Sami Rouhe <rouhesami@gmail.com>
2019-12-05 16:57:38 +0200

    utils

--- utils/new_project.sh
+++ utils/new_project.sh
@@ -13,6 +13,9 @@ NORMAL="\033[0;39m"
 printf "$WHITE%s$NORMAL" "Project name: "
 read project
 
+printf "$WHITE%s$NORMAL" "Author: "
+read author
+
 mkdir $project
 printf "$CYAN%s %s$NORMAL\n" "Initializing" "$project"
 git clone https://github.com/Rouhija/libft templibft
@@ -21,6 +24,7 @@ rm -rf $project/libft/.git
 mkdir $project/srcs
 mkdir $project/includes
 echo ".DS_Store\n*.a\n*.o\na.out\n.vscode" > $project/.gitignore
+echo $author > $project/author
 touch $project/srcs/$project.c
 touch $project/includes/$project.h
 
